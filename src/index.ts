ae8c5d6e88a0c797c4f67cf8e1e027099b97d8d6
Stanislav Volnjanskij <stas@kultprosvet.net>
2019-08-06 13:41:12 +0300

    export fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import {createBaseCrudResolver} from "./backend-helper/BaseAdminResourceResolver";
-import {EntityUpdateHelper} from './backend-helper/EntityUpdateHelper'
-export default {
-    createBaseCrudResolver,
-    EntityUpdateHelper
-}
+export * from "./backend-helper/BaseAdminResourceResolver";
+export * from './backend-helper/EntityUpdateHelper'
