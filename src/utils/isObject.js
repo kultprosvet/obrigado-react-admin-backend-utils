e3a31be108e42491e7b775f20524a67dfe3a8422
Stanislav Volnjanskij <stas@kultprosvet.net>
2020-01-09 15:33:32 +0200

    removed js in src

--- src/utils/isObject.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isObject(value) {
-    return value && typeof value === 'object' && value.constructor === Object;
-}
-exports.isObject = isObject;
