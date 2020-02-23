bee47fb52c30ffa4626612c74f3475d6ea4d49e8
Stanislav Volnjanskij <stas@kultprosvet.net>
2020-02-23 11:28:42 +0200

    1.1.29 Migration fixed

--- src/migrations/1566135244000-AddAdmin.ts
+++ src/migrations/1566135244000-AddAdmin.ts
@@ -1,17 +1,19 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
+import { createQueryBuilder, MigrationInterface, QueryRunner } from "typeorm";
 import {Administrator} from "../models/Administrator";
 import * as bcrypt from 'bcrypt'
 export class AddAdmin1566135244000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
-
-        let admin=await Administrator.findOne({where:{username:'admin'}})
+        let admin=await createQueryBuilder(Administrator).select("username").where("username=:name",{name:'admin'}).getRawOne()
         if (admin==null) {
-            admin = new Administrator()
-            admin.first_name = "Admin"
-            admin.last_name = 'Admin'
-            admin.username = 'admin'
-            admin.password = bcrypt.hashSync("admin#12345", 10)
-            await admin.save()
+            await createQueryBuilder()
+                .insert()
+                .into(Administrator)
+                .values({
+                    first_name: 'Admin',
+                    last_name: 'Admin',
+                    username: 'admin',
+                    password: bcrypt.hashSync('admin#12345', 10),
+                }).execute()
         }
     }
 
